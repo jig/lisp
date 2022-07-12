f5e76d69950bd75e7d699cc607d580625f2e73ec
Jordi Iñigo <jordi.inigo@entrust.com>
2022-07-12 14:53:18 +0200

    Test file helper function

--- testlib/testlib.go
+++ testlib/testlib.go
@@ -23,32 +23,36 @@ func Directory(t *testing.T, directory embed.FS, packages PackageDecl) error {
 	}
 	for _, entry := range d {
 		t.Run(entry.Name(), func(t *testing.T) {
-			tenv, err := env.NewEnv(nil, nil, nil)
-			if err != nil {
-				t.Fatalf("Environment Setup Error: %v\n", err)
-			}
-			for _, library := range packages {
-				if err := library.Load(tenv); err != nil {
-					log.Fatalf("Library Load Error: %v\n", err)
-				}
-			}
 			testFile, err := directory.ReadFile(entry.Name())
 			if err != nil {
 				t.Fatalf("%s/ReadFile Error: %s", entry.Name(), err)
 			}
-			expr, err := lisp.READ(string(testFile), nil)
-			if err != nil {
-				t.Fatalf("%s/READ Error: %s", entry.Name(), err)
-			}
-			ctxt := context.Background()
-			res, err := lisp.EVAL(expr, tenv, &ctxt)
-			if err != nil {
-				t.Fatalf("%s/EVAL Error: %s", entry.Name(), err)
-			}
-			if res, ok := res.(bool); !res || !ok {
-				t.Fatalf("%s/TEST failed", entry.Name())
-			}
+			File(t, entry.Name(), string(testFile), packages)
 		})
 	}
 	return nil
+}
+
+func File(t *testing.T, entryName, testFile string, packages PackageDecl) {
+	tenv, err := env.NewEnv(nil, nil, nil)
+	if err != nil {
+		t.Fatalf("Environment Setup Error: %v\n", err)
+	}
+	for _, library := range packages {
+		if err := library.Load(tenv); err != nil {
+			log.Fatalf("Library Load Error: %v\n", err)
+		}
+	}
+	expr, err := lisp.READ(string(testFile), nil)
+	if err != nil {
+		t.Fatalf("%s/READ Error: %s", entryName, err)
+	}
+	ctxt := context.Background()
+	res, err := lisp.EVAL(expr, tenv, &ctxt)
+	if err != nil {
+		t.Fatalf("%s/EVAL Error: %s", entryName, err)
+	}
+	if res, ok := res.(bool); !res || !ok {
+		t.Fatalf("%s/TEST failed", entryName)
+	}
 }
