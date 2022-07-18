8cdcea97502c53e1562e0ec041ac99975188f30e
Jordi Iñigo <jordi.inigo@entrust.com>
2022-07-18 14:47:08 +0200

    Disabled verbose test

--- trycatchfinally_test.lisp
+++ trycatchfinally_test.lisp
@@ -1,20 +1,21 @@
 ;; uncomment to test
 
-(try
-    (prn 1)
-    (prn 2)
-    (prn 3)
-    (throw true)
-    (catch e
-        (prn 4)
-        (prn 5)
-        (prn 6)
-        (prn {
-            :err e
-            :desc "miserable error"
-        })
-        e)
-    (finally
-        (prn 7)
-        (prn 8)
-        (prn 9)))
+;; (try
+;;     (prn 1)
+;;     (prn 2)
+;;     (prn 3)
+;;     (throw true)
+;;     (catch e
+;;         (prn 4)
+;;         (prn 5)
+;;         (prn 6)
+;;         (prn {
+;;             :err e
+;;             :desc "miserable error"
+;;         })
+;;         e)
+;;     (finally
+;;         (prn 7)
+;;         (prn 8)
+;;         (prn 9)))
+true
