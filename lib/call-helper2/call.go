575fbd597f12ed4d9cd79367fbbe0f46f5803317
Jordi Íñigo <jordi.inyigo@gmail.com>
2022-07-19 10:13:43 +0200

    New caller stubs (examples and tests only)

--- lib/call-helper2/call.go
+++ lib/call-helper2/call.go
@@ -1,18 +1,111 @@
 package call2
 
 import (
+	"context"
+	"fmt"
 	"reflect"
 )
 
-func Call(fin interface{}, args ...interface{}) (interface{}, error) {
+func Call(fin interface{}) func(context.Context, ...interface{}) (interface{}, error) {
 	finType := reflect.TypeOf(fin)
 	finValue := reflect.ValueOf(fin)
-
 	inParams := finType.NumIn()
+	outParams := finType.NumOut()
+
+	switch finType.NumOut() {
+	case 0:
+		return func(_ context.Context, args ...interface{}) (result interface{}, err error) {
+			malRecover(&err)
+			return _nil_nil(finValue.Call(_args(inParams, args)))
+		}
+	case 1:
+		return func(_ context.Context, args ...interface{}) (result interface{}, err error) {
+			malRecover(&err)
+			return _nil_error(finValue.Call(_args(inParams, args)))
+		}
+	case 2:
+		return func(_ context.Context, args ...interface{}) (result interface{}, err error) {
+			malRecover(&err)
+			return _result_error(finValue.Call(_args(inParams, args)))
+		}
+	default:
+		panic(fmt.Sprintf("wrong number of results (%d instead of 2)", outParams))
+	}
+}
+
+func CallWithContext(fin interface{}) func(context.Context, ...interface{}) (interface{}, error) {
+	finType := reflect.TypeOf(fin)
+	finValue := reflect.ValueOf(fin)
+	inParams := finType.NumIn()
+	outParams := finType.NumOut()
+
+	switch finType.NumOut() {
+	case 0:
+		return func(ctx context.Context, args ...interface{}) (result interface{}, err error) {
+			malRecover(&err)
+			return _nil_nil(finValue.Call(_args_ctx(ctx, inParams, args)))
+		}
+	case 1:
+		return func(ctx context.Context, args ...interface{}) (result interface{}, err error) {
+			malRecover(&err)
+			return _nil_error(finValue.Call(_args_ctx(ctx, inParams, args)))
+		}
+	case 2:
+		return func(ctx context.Context, args ...interface{}) (result interface{}, err error) {
+			malRecover(&err)
+			return _result_error(finValue.Call(_args_ctx(ctx, inParams, args)))
+		}
+	default:
+		panic(fmt.Sprintf("wrong number of results (%d instead of 2)", outParams))
+	}
+}
+
+func malRecover(err *error) {
+	rerr := recover()
+	if rerr != nil {
+		*err = rerr.(error)
+	}
+}
+
+func _args_ctx(ctx context.Context, inParams int, args []interface{}) []reflect.Value {
+	if len(args) != inParams-1 {
+		panic(fmt.Sprintf("wrong number of arguments (%d instead of %d)", len(args), inParams))
+	}
+
+	in := make([]reflect.Value, inParams)
+	in[0] = reflect.ValueOf(ctx)
+	for k, param := range args {
+		in[k+1] = reflect.ValueOf(param)
+	}
+	return in
+}
+
+func _args(inParams int, args []interface{}) []reflect.Value {
+	if len(args) != inParams {
+		panic(fmt.Sprintf("wrong number of arguments (%d instead of %d)", len(args), inParams))
+	}
+
 	in := make([]reflect.Value, inParams)
 	for k, param := range args {
 		in[k] = reflect.ValueOf(param)
 	}
-	res := finValue.Call(in)
-	return res[0].Interface(), nil
+	return in
+}
+
+func _nil_nil(res []reflect.Value) (result interface{}, err error) {
+	return nil, nil
+}
+
+func _nil_error(res []reflect.Value) (result interface{}, err error) {
+	if res[0].Interface() == nil {
+		return nil, nil
+	}
+	return nil, res[0].Interface().(error)
+}
+
+func _result_error(res []reflect.Value) (result interface{}, err error) {
+	if res[1].Interface() == nil {
+		return res[0].Interface(), nil
+	}
+	return res[0].Interface(), res[1].Interface().(error)
 }
--- lib/call-helper2/call_test.go
+++ lib/call-helper2/call_test.go
@@ -1,20 +1,70 @@
 package call2
 
 import (
+	"context"
 	"errors"
-	"fmt"
 	"testing"
+	"time"
 )
 
-func TestF(t *testing.T) {
-	result, error := Call(Div, 2, 6)
-	fmt.Println("res", result)
-	fmt.Println("err", error)
+func TestOK(t *testing.T) {
+	fDiv := Call(divExample)
+	result, err := fDiv(context.TODO(), 2, 6)
+	if result.(int) != 8 || err != nil {
+		t.Fatal("test failed")
+	}
 }
 
-func Div(a, b int) (int, error) {
+func TestNoOKResult(t *testing.T) {
+	fDiv := Call(divExample)
+	_, err := fDiv(context.TODO(), 2, 0)
+	if err.Error() != "divide by zero" {
+		t.Fatal("test failed")
+	}
+}
+
+func TestNoOKArguments(t *testing.T) {
+	fDiv := Call(divExample)
+	defer func() {
+		rerr := recover()
+		if rerr.(string) != "wrong number of arguments (3 instead of 2)" {
+			t.Fatal("test failed")
+		}
+	}()
+	_, _ = fDiv(context.TODO(), 2, 3, 4)
+}
+
+func TestOKWithContext(t *testing.T) {
+	fSleep := CallWithContext(sleepExample)
+	_, err := fSleep(context.Background(), 10)
+	if err != nil {
+		t.Fatal("test failed")
+	}
+}
+
+func TestOKWithContextTimeout(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+
+	fSleep := CallWithContext(sleepExample)
+	_, err := fSleep(ctx, 20)
+	if err != nil {
+		t.Fatal("test failed")
+	}
+}
+
+func divExample(a, b int) (int, error) {
 	if b == 0 {
 		return 0, errors.New("divide by zero")
 	}
 	return a + b, nil
+}
+
+func sleepExample(ctx context.Context, ms int) error {
+	select {
+	case <-ctx.Done():
+		return errors.New("timeout while evaluating expression")
+	case <-time.After(time.Millisecond * time.Duration(ms)):
+		return nil
+	}
 }
