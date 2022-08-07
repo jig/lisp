af13f6894bfa75229804c5e75448ca9665026140
Jordi Íñigo <jordi.inyigo@gmail.com>
2022-08-07 17:11:35 +0200

    Suport of F12 on debugger (out)

--- examples/simple.lisp
+++ examples/simple.lisp
@@ -1,5 +1,8 @@
 (def a 1)
 (def b (fn [x] x))
-(if (= a 1)
-    (prn a (b 2))
-    (prn "hello"))
+(def c (fn [x]
+    (if (= a 1)
+        (prn a (b 2))
+        (prn "hello"))))
+(c 3)
+(prn "end")
