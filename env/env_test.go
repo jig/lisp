de096c5f8d1123636a18d94882d39c78fc2c9f76
Víctor Terraga Gómez <40562322+victorterraga@users.noreply.github.com>
2022-09-29 21:50:36 +0200

    tests shared vars in ens (#28)
    Co-authored-by: Víctor Terraga <victor.terraga@entrust.com>

--- env/env_test.go
+++ env/env_test.go
@@ -18,3 +18,34 @@ func TestEnv(t *testing.T) {
 		t.Fatal()
 	}
 }
+
+func TestSubordEnv(t *testing.T) {
+	year := types.Symbol{Val: "year"}
+	ns := NewEnv()
+
+	s1env := NewSubordinateEnv(ns)
+	s1env.Set(year, 1984)
+
+	s2env := NewSubordinateEnv(ns)
+	s2env.Set(year, 1985)
+
+	res, err := s1env.Get(year)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.(int) != 1984 {
+		t.Fatal()
+	}
+
+	res2, err := s2env.Get(year)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res2.(int) != 1985 {
+		t.Fatal()
+	}
+
+	if _, err := ns.Get(year); err == nil {
+		t.Fatal("should not find symbol")
+	}
+}
