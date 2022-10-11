36985595373a9b385877a832d5ed39f12b95713e
Jordi Íñigo <jordi.inyigo@gmail.com>
2022-10-11 22:53:14 +0200

    Actions bumped to Go 1.19.2

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -21,7 +21,7 @@ su user
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install go
-VERSION='1.18'
+VERSION='1.19.2'
 OS='linux'
 ARCH='amd64'
 
