b4cb1f08f30df21c657b716749269d0bfb279d31
Jordi Íñigo <jordi.inigo@entrust.com>
2025-03-01 22:07:19 +0100

    First dagger actions (interim)

---
+++ dagger/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"dagger/lisp/internal/dagger"
+)
+
+type Lisp struct{}
+
+// Setup cached dependencies
+func (m *Lisp) Setup(source *dagger.Directory) *dagger.Container {
+	return dag.Container().
+		From("golang:1.24").
+		WithDirectory("/lisp", source).
+		WithWorkdir("/lisp").
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod-124")).
+		WithEnvVariable("GOMODCACHE", "/go/pkg/mod").
+		WithMountedCache("/go/build-cache", dag.CacheVolume("go-build-124")).
+		WithEnvVariable("GOCACHE", "/go/build-cache")
+}
+
+// Build lisp interpreter
+func (m *Lisp) Build(source *dagger.Directory) *dagger.Container {
+	return m.Setup(source).
+		WithExec([]string{"go", "build", "-o", "/lisp/lisp", "cmd/lisp/main.go"})
+}
+
+// Execute all tests
+func (m *Lisp) Test(source *dagger.Directory) *dagger.Container {
+	return m.Build(source).
+		WithExec([]string{"go", "test", "./..."})
+}
+
+// Execute all benchmarks
+func (m *Lisp) Benchmark(source *dagger.Directory) *dagger.Container {
+	return m.Build(source).
+		WithExec([]string{"go", "test", "--bench=.", "./..."})
+}
