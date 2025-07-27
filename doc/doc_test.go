aa4c4c6de4ec6462a6635d3b9a5ceea9072c9941
Jordi Íñigo Griera <jig@users.noreply.github.com>
2025-07-27 08:25:27 +0200

    Added more libraries

--- doc/doc_test.go
+++ doc/doc_test.go
@@ -1,21 +1,34 @@
 package doc
 
 import (
-	_ "embed" // for embedding files
+	"embed"
 	"encoding/json"
-	"fmt"
 	"testing"
 )
 
-//go:embed core.json
-var docCore []byte
+//go:embed *.json
+var allJSONFiles embed.FS
 
 func TestDocDecode(t *testing.T) {
-	// Test decoding of the core documentation
-	var docsLib Library
-	err := json.Unmarshal(docCore, &docsLib)
+	files, err := allJSONFiles.ReadDir(".")
 	if err != nil {
-		t.Fatalf("Failed to decode core documentation: %v", err)
+		t.Fatalf("Failed to read embedded files: %v", err)
 	}
-	fmt.Println("Core documentation decoded successfully with", len(docsLib.Symbols), "entries")
+
+	for _, file := range files {
+		t.Run(file.Name(), func(t *testing.T) {
+			data, err := allJSONFiles.ReadFile(file.Name())
+			if err != nil {
+				t.Errorf("Failed to read file %s: %v", file.Name(), err)
+				return
+			}
+
+			// Test decoding of the core documentation
+			var docsLib Library
+			if err := json.Unmarshal(data, &docsLib); err != nil {
+				t.Fatalf("Failed to decode core documentation: %v", err)
+			}
+			t.Logf("Library %q documentation decoded successfully with %d entries", file.Name(), len(docsLib.Symbols))
+		})
+	}
 }
