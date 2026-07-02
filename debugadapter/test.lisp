1db42463a0e534b05258a46d9ae1e202a183a5a3
Jordi Íñigo Griera <jig@users.noreply.github.com>
2026-07-02 09:33:26 +0200

    Debugging example a bit richer

--- debugadapter/test.lisp
+++ debugadapter/test.lisp
@@ -1,6 +1,10 @@
 ;; test.lisp
 
 ;; this is a simple code to test the lisp-debugger
+
+(defn sqr [x]
+    (* x x))
+
 (do
     (def a 33)
     (println 1)
@@ -9,6 +13,6 @@
     (if (= 1 2)
         (println 4)
         (do
-            (def b 44)
+            (def b (sqr a))
             (println b))
     ))
